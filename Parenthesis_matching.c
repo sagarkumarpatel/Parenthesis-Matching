322348e26c1932cd11c6631d3acdb8942a050de4
sagarkumarpatel <sagarpatel98212@gmail.com>
2025-03-13 14:39:28 +0530

    Added createStack function to dynamically allocate memory for the stack and initialize its properties (top, size, and arr). Includes error handling for memory allocation failure.

--- Parenthesis_matching.c
+++ Parenthesis_matching.c
@@ -7,6 +7,17 @@ struct stack{
     char *arr;
 
 };
+struct stack* createStack(int size){
+    struct stack* s=(struct stack*)malloc(sizeof(struct stack));
+    if(s==NULL){
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    s->top=-1;
+    s->size=size;
+    s->arr=(char*)malloc(size * sizeof(char));
+    return s;
+}
 int main(){
 
 
